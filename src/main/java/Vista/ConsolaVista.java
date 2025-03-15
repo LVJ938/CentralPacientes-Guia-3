4b79c0d09ab1266d623f3f434ff76aa8e155f510
Luis <luisvj.dev@gmail.com>
2025-03-15 17:18:11 -0500

    Se crea el menu en el paquete vista para que el usuario interactue con el aplicativo se crea la clase ConsolaVista. faltan pruevas de funcionalidad

--- src/main/java/Vista/ConsolaVista.java
+++ src/main/java/Vista/ConsolaVista.java
@@ -3,6 +3,47 @@ package Vista;
  *
  * @author Luis Vargas
  */
+
+import java.util.Scanner;
+
 public class ConsolaVista {
+    private Scanner input;
     
+    public ConsolaVista(){
+        input = new Scanner(System.in); // indica que el Scanner toma los datos ingresados por consola
+    }
+    
+    public int mostrarMenu(){
+        System.out.println("\n--- Gestión de Pacientes ---");
+        System.out.println("1. Agregar Paciente");
+        System.out.println("2. Buscar Paciente");
+        System.out.println("3. Eliminar Paciente");
+        System.out.println("4. Mostrar los Pacientes")
+        System.out.println("5. Salir");
+        System.out.print("Seleccione una opción: ");
+        return input.nextInt(); // Retorna la opcion leida por teclado para que el controlador la procese.
+    }
+    
+    // Metodos para pedir los datos del usuario cada dato se solicita en un método diferente para hacerlo mas flexible 
+    
+    public int pedirId(){
+        System.out.println("Ingrese el numero de identificacion del paciente");
+        return input.nextInt();
+    }
+     public int pedirEdad(){
+        System.out.println("Ingrese la edad del paciente");
+        return input.nextInt();
+    }
+     public String pedirNombre(){
+        System.out.println("Ingrese el nombre del paciente");
+        return input.next();
+    }
+    public String pedirClinica(){
+        System.out.println("Ingrese la clinica del paciente");
+        return input.next();
+    }
+    
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
 }
